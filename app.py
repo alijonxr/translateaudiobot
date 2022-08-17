86cbf720034cf957c5f22919fd89c08f307500ea
Muhammadrizo <87116724+MuhammadrizoDeveloper@users.noreply.github.com>
2022-08-17 22:33:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ import middlewares, filters, handlers
 from utils.set_bot_commands import set_default_commands
 
 if __name__ == '__main__':
-    executor.start_webhook(dp)
+    executor.start_polling(dp)
